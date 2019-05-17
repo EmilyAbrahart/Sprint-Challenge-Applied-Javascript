f1b040b2472b25fa627445bdb87742a208fe446a
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-17 11:06:17 +0100

    Attempt carousel stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,20 +3,27 @@ class Carousel {
   constructor(carousel) {
     this.carousel = carousel;
     this.imgs = this.carousel.querySelectorAll('img');
+    this.currentImgIndex = 0;
     this.leftBtn = this.carousel.querySelector('.left-button');
     this.rightBtn = this.carousel.querySelector('right-button');
 
-    this.leftBtn.addEventListener('click', this.changeImage());
-    this.rightBtn.addEventListener('click', this.changeImage());
+    this.leftBtn.addEventListener('click', this.changeImage('left'));
+    this.rightBtn.addEventListener('click', this.changeImage('right'));
   }
 
-  changeImage(){
-      const activeImg = this.carousel.querySelector('.active-img')
+  changeImage(direction) {
+    const imgArray = [...this.imgs];
+    let activeImg = imgArray[this.currentImgIndex];
+    activeImg.style.display = 'block';
+    if (direction === 'right') {
+      activeImg = imgArray[this.currentImgIndex + 1];
+    } else {
+      activeImg = imgArray[this.currentImgIndex - 1];
+    }
   }
 }
 
-const carousel = document.querySelector('.carousel');
-new Carousel(carousel);
+const carousel = document.querySelector('.carousel').forEach(carousel => new Carousel(carousel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
